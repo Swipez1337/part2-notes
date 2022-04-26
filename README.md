510068500522a62ebc27bf6acaada71459d47265
Swipez1337 <frederiklundsbjerg@hotmail.com>
2022-04-26 18:01:57 +0200

    Exercise 2.6 solution

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Notes app
+
+Written by Frederik Lundsbjerg, s205472
+
+My explanation of my changes to the project. 
+
+As explained in the last line above the assignment in the link https://fullstackopen.com/en/part2/forms#exercises-2-6-2-10 
+I have taken that starting point from the code in branch "part2-3", found here https://github.com/fullstack-hy2020/part2-notes/tree/part2-3
+
--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
 import { useState } from 'react'
-import Note from './components/Note'
+import Name from './components/Name'
+import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
+    const [names, setNames] = useState(props.names)
+    const [newName, setNewName] = useState('')
+    const [showAll, setShowAll] = useState(true)
+
+
+    const addName = (event) => {
+        event.preventDefault()
+        const nameObject = {
+            content: newName,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: names.length + 1,
+        }
+
+        setNames(names.concat(nameObject))
+        setNewName('')
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    const namesToShow = showAll
+        ? names
+        : names.filter(name => name.important)
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>   
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName}
+                                 onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {namesToShow.map(name =>
+                    <Name key={name.id} name={name} />
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default App
